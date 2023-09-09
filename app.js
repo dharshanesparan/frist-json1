0ed2e4fcbbee98e7e76afcbc4af731b774e11211
dharshanesparan <dharshanpa.esparan644@gmail.com>
2023-09-09 15:23:18 +0530

    updata

---
+++ app.js
@@ -0,0 +1,34 @@
+let json1="Dharshan"
+let json2= 4
+let json3=true
+let json4=[4,5,6,]
+let json5={
+    "stock ":"Tcs",
+    "price":34000
+}
+let json6=[
+
+    {"stock ":"Tcs",
+    "price":34000
+
+    },
+
+    {
+
+ "Name ":"Dharshan",
+ "Age":20
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(json1,json2,json3,json4,json5,json6)
